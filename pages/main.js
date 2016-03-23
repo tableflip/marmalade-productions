d0a5acdd5249b3ec12f6a6ecc9fcc24173380734
Bernard Mordan <bernardmordan@gmail.com>
2016-03-23 13:22:56 +0000

    scopes css, targets video and improves event handling

--- pages/main.js
+++ pages/main.js
@@ -7,18 +7,24 @@ $('.carousel-item').first().addClass('active')
 $('#clients-carousel').carousel()
 
 $('#play').on('click', function (e) {
+  e.preventDefault()
   $('.container-lead').addClass('video-playing')
   $('.container-video').show().on('click', stopVideo)
   player('play')
 })
 
+function removeStopVideo () {
+  $('.container-video').off('click', stopVideo)
+}
+
 function stopVideo () {
   $('.container-lead').removeClass('video-playing')
-  $('.container-video').hide().off('click', this)
+  $('.container-video').hide()
   player('unload')
+  removeStopVideo()
 }
 
 function player (opt) {
   var opts = JSON.stringify({ 'method': opt })
-  $('iframe').get(0).contentWindow.postMessage(opts, '*')
+  $('#video').get(0).contentWindow.postMessage(opts, '*')
 }
