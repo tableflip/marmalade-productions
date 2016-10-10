f4d7247ea7e62f82b8c6edd75dbf1faa0958306c
Bernard Mordan <bernardmordan@gmail.com>
2016-10-10 15:17:53 +0100

    adds vimeo client

--- pages/main.js
+++ pages/main.js
@@ -1,4 +1,7 @@
 var $ = window.$ = window.jQuery = require('jquery')
+var Vimeo = require('@vimeo/player/dist/player.min.js')
+var player = new Vimeo.Player($('#video'))
+
 require('bootstrap/dist/js/umd/carousel.js')
 require('bootstrap/dist/js/umd/collapse.js')
 require('./rolodex.js')()
@@ -9,7 +12,7 @@ $('body').on('click', stopVideo)
 function startVideo (e) {
   e.preventDefault()
   e.stopPropagation()
-  player('play')
+  player.play()
   $('.container-lead').addClass('fade').one('transitionend', function (evt) {
     $(this).addClass('gone')
   })
@@ -19,11 +22,11 @@ function stopVideo () {
   var $lead = $('.container-lead')
   if (!$lead.hasClass('gone')) return
   $('.container-lead').removeClass('gone fade').one('transitionend', function (evt) {
-    player('unload')
+    player.unload()
   })
 }
 
-function player (opt) {
-  var opts = JSON.stringify({ 'method': opt })
-  $('#video').get(0).contentWindow.postMessage(opts, '*')
-}
+// function player (opt) {
+//   var opts = JSON.stringify({ 'method': opt })
+//   $('#video').get(0).contentWindow.postMessage(opts, '*')
+// }
